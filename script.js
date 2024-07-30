0b774056dc61435b1cf6063d73bb61cc5d7aa778
Gerson Cesar Grobe de Miranda <106871453+cesargrobe@users.noreply.github.com>
2024-07-30 19:18:45 -0300

    Aula 30/07 2

--- script.js
+++ script.js
@@ -34,6 +34,8 @@ let perguntaAtual;
 function mostraPergunta() {
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
+    mostraAlternativas();
 }
 
 function mostraAlternativas(){
