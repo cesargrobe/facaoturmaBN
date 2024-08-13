b4d3d0084d108438e2c8bc39dbe5005cb1fa4122
Gerson Cesar Grobe de Miranda <106871453+cesargrobe@users.noreply.github.com>
2024-08-13 19:19:46 -0300

    Aula 13/08 5

--- script.js
+++ script.js
@@ -51,6 +51,7 @@ const perguntas = [
 
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
 
 function mostraPergunta() {
     perguntaAtual = perguntas[atual];
@@ -62,11 +63,16 @@ function mostraAlternativas() {
     for (const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
-        botaoAlternativas.addEventListener("click", function () {
-            atual++;
-            mostraPergunta();
-        })
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
         caixaAlternativas.appendChild(botaoAlternativas);
     }
 }
+
+function respostaSelecionada(opcaoSelecionada){
+    const afirmacao = opcaoSelecionada.afirmacao;
+    historiaFinal = afirmacao;
+    atual++;
+    mostraPergunta();
+}
+
 mostraPergunta();
